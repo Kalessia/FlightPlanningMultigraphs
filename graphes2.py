f64b24a12a999fbe6365d873db955c8c3a3bf708
Kalessia <73894713+Kalessia@users.noreply.github.com>
2021-12-04 17:20:13 +0100

    x

--- graphes2.py
+++ graphes2.py
@@ -147,7 +147,35 @@ class Graph:
 			#return "Erreur algorithme BFS"
 		print("arbreCouvrante :", arbreCouvrante)
 		self.arbreCouvrante = arbreCouvrante # dictionnaire
-		return arbreCouvrante	
+		return arbreCouvrante
+
+	# Méthode permettant d'afficher à l'écran un graphe non orienté et, éventuellement, un titre
+	def showGraphe(self, titre = "G"):
+		# """ G : un dictionnaire representant un graphe { sommet s : sommets adjacents à s}
+		#     titre : titre du graphe à afficher, 'G' par defaut
+		# """
+		newG = nx.DiGraph()
+		listeSommetsG = list(self.adjacency_list.keys())
+		newG.add_nodes_from(listeSommetsG)
+
+		listeW = []
+		for source in listeSommetsG:
+			for dest , w in self.adjacency_list[source]:
+				newG.add_edge(source, dest, weight=w)
+				listeW.append(w)
+
+		plt.title(titre)
+		pos = nx.circular_layout(newG)
+		e_labels = nx.get_edge_attributes(newG,'weight')
+		nx.draw_networkx_edge_labels(newG, pos=pos, edge_labels=e_labels)
+		nx.draw(newG, with_labels=True, node_size=1500, pos=pos)
+
+		plt.show()   
+
+
+
+
+#showGrapheCouvrant(g, titre = "G Couvrant")	
 
 
 
@@ -185,6 +213,68 @@ class Graph:
 		return path
 
 
+
+
+def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723 
+
+    If the graph is a tree this will return the positions to plot this in a 
+    hierarchical layout.
+
+    G: the graph (must be a tree)
+
+    root: the root node of current branch 
+    - if the tree is directed and this is not given, the root will be found and used
+    - if the tree is directed and this is given, then the positions will be just for the descendants of this node.
+    - if the tree is undirected and not given, then a random choice will be used.
+
+    width: horizontal space allocated for this branch - avoids overlap with other branches
+
+    vert_gap: gap between levels of hierarchy
+
+    vert_loc: vertical location of root
+
+    xcenter: horizontal location of root
+    '''
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        '''
+        see hierarchy_pos docstring for most arguments
+
+        pos: a dict saying where all nodes go if they have been assigned
+        parent: parent of this branch. - only affects it if non-directed
+
+        '''
+
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)  
+        if len(children)!=0:
+            dx = width/len(children) 
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
 
 ####################################################################################################################
@@ -458,68 +548,17 @@ def shortest(x, y, matG, verbose):
 
 	return path
 
-#-------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 ####################################################################################################################
-#	TESTS DEBUG
+#	AFFICHAGE COURBES et GRAPHES
 ####################################################################################################################
-
-mg = acquisitionMultigraphe("multigrapheG1.txt")
-
-if mg != None :
-	mg.afficheMultigraphe()
-print("------------------------------------------------------------------")
-
-#transformationMultigrapheMatAdjacence(g, verbose = False)
-#print("------------------------------------------------------------------")
-
-#listeAdj = transformationMultigrapheListeAdjacence(g, verbose = False)
-#print("------------------------------------------------------------------")
-
-g = mg.transform_to_graph(True)
-print(g.adjacency_list)
-print("------------------------------------------------------------------")
-
-g.BFS("a", "g", [0, 10], True)
-#print("------------------------------------------------------------------")
-
-
-earliest_arrival('a', 'g', g, verbose = True)
-
-
-
-
-
-
 
 import networkx as nx
 import matplotlib.pyplot as plt
 import time
 import datetime
 
-# Méthode permettant d'afficher à l'écran un graphe non orienté et, éventuellement, un titre
-def showGraphe(G, titre = "G"):
-    """ G : un dictionnaire representant un graphe { sommet s : sommets adjacents à s}
-        titre : titre du graphe à afficher, 'G' par defaut
-    """
-    newG = nx.Graph()
-    newG.add_nodes_from(list(G.keys()))
-    for v1 in G.keys() :
-        for v2 in G.keys() :
-            if (v2, v1) not in newG.edges and v2 in G[v1]:
-                newG.add_edge(v1, v2)
 
-    plt.title(titre)
-    nx.draw(newG, with_labels=True, node_size=1500, node_color="skyblue", pos=nx.circular_layout(G))
-
-    plt.show()   
 
 #------------------------------------------------------------------------------------------------------
 
@@ -754,3 +793,42 @@ def plotRapportApproximation(nMax, p, mode, verbose = False, save = False):
         plt.savefig("TestResults/rapportApproximation_p=" + str(p) + "_" + str(datetime.date.today()) + str(datetime.datetime.now().strftime("_%H_%M_%S")) + ".jpeg", transparent = True)
 
     plt.show()
+
+
+
+
+
+
+####################################################################################################################
+#	TESTS DEBUG
+####################################################################################################################
+
+mg = acquisitionMultigraphe("multigrapheG1.txt")
+
+if mg != None :
+	mg.afficheMultigraphe()
+print("------------------------------------------------------------------")
+
+#transformationMultigrapheMatAdjacence(g, verbose = False)
+#print("------------------------------------------------------------------")
+
+#listeAdj = transformationMultigrapheListeAdjacence(g, verbose = False)
+#print("------------------------------------------------------------------")
+
+g = mg.transform_to_graph(True)
+#print(g.adjacency_list)
+#print("------------------------------------------------------------------")
+
+#g.BFS("a", "g", [0, 10], True)
+#print("------------------------------------------------------------------")
+
+#earliest_arrival('a', 'g', g, verbose = True)
+#print("------------------------------------------------------------------")
+
+g.showGraphe(titre = "graphe issu du multigraphe")
+#print("------------------------------------------------------------------")
+
+#showGrapheCouvrant(g, titre = "G Couvrant")
+#print("------------------------------------------------------------------")
+
+
